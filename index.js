beafdca86819723d5e2538938c3ab62edd97aec2
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-02 22:32:56 +0100

    Fix Linters errors

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer();
-
 const server = http.createServer((req, res) => {
-    const method = req.method;
-    const current_url = new URL(req.url);
-    const pathname = current_url.pathname;
-    const search_params = current_url.searchParams;
+  const { method } = req;
+  const currentUrl = new URL(req.url);
+  const { pathname } = currentUrl;
+  // const searchParams = currentUrl.searchParams;
 
-    if(method === 'GET' && pathname === '/' && pathname === '') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream('./index.html').pipe(res);
-
-    }
-    else if(method === 'GET' && pathname === '/about') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream('./about.html').pipe(res);
-    }
-    else if(method === 'GET' && pathname === '/contact-me') {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.createReadStream('./contact-me.html').pipe(res);
-    }
-    else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        fs.createReadStream('./404.html').pipe(res);
-    }
-    
-    
-}); 
+  if (method === 'GET' && pathname === '/' && pathname === '') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream('./index.html').pipe(res);
+  } else if (method === 'GET' && pathname === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream('./about.html').pipe(res);
+  } else if (method === 'GET' && pathname === '/contact-me') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.createReadStream('./contact-me.html').pipe(res);
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    fs.createReadStream('./404.html').pipe(res);
+  }
+});
 
 server.listen(8080);
