db33e150af3eaccd900a5195eafffaacc360c1a8
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2022-01-02 22:48:44 +0100

    Handle URL with req.url insteal of new URL(...)

--- index.js
+++ index.js
@@ -3,17 +3,16 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
   const { method } = req;
-  const currentUrl = new URL(req.url);
-  const { pathname } = currentUrl;
-  // const searchParams = currentUrl.searchParams;
 
-  if (method === 'GET' && pathname === '/' && pathname === '') {
+    // const searchParams = currentUrl.searchParams;
+
+  if (method === 'GET' && (req.url === '/' || req.url === '')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.createReadStream('./index.html').pipe(res);
-  } else if (method === 'GET' && pathname === '/about') {
+  } else if (method === 'GET' && req.url === '/about') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.createReadStream('./about.html').pipe(res);
-  } else if (method === 'GET' && pathname === '/contact-me') {
+  } else if (method === 'GET' && req.url === '/contact-me') {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.createReadStream('./contact-me.html').pipe(res);
   } else {
